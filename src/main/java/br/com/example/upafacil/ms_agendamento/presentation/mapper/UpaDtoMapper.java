846055f67deca21fda6cc7acbf2a6ad1cfc78f0b
felipewiiu3 <felipewiiu3@gmail.com>
2025-02-18 17:10:02 -0300

    config: Configura o mappstruct

--- src/main/java/br/com/example/upafacil/ms_agendamento/presentation/mapper/UpaDtoMapper.java
+++ src/main/java/br/com/example/upafacil/ms_agendamento/presentation/mapper/UpaDtoMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface UpaDtoMapper {
 
-    @Mapping(target =   "upaId", ignore = true)
     UpaDto toDto(Upa upa);
 
     Upa toDomain(UpaDto upaDto);
